9258c6fe26b6ec8c9021f5e6872fb0f993022219
Devin Gould <djgould0628@gmail.com>
2023-03-12 18:33:07 +0800

    ship

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   });
   app.useLogger(app.get(Logger));
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 
   const storage = multer.diskStorage({
     destination: (req, file, cb) => {
