cfe49eaa302d81456f8885dd71935a8992581725
Devin Gould <djgould0628@gmail.com>
2023-06-29 18:24:33 -0600

    ship

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -62,6 +62,7 @@ export class StripeService {
   async createSession(orderId: string, returnUrl: string, cancelUrl: string) {
     return await this.stripe.checkout.sessions.create({
       payment_method_types: ['card'],
+      allow_promotion_codes: true,
       line_items: [
         {
           price_data: {
