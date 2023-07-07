759898035ee4bc6bed01399540b996d48d276880
Devin Gould <djgould0628@gmail.com>
2023-07-07 09:03:40 -0600

    ship

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
+import { AuthGuard } from 'src/auth/auth.guard';
 import { CurrentUser } from './user.decorator';
 
 @Controller('user')
+@UseGuards(AuthGuard)
 export class UserController {
   @Get('/me')
   async getMe(@CurrentUser() user: User) {
